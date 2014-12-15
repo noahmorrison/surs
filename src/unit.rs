6bca872c78a58905091734560b7df6b968f1e225
noah morrison <noah@morrison.ph>
2014-12-15 18:03:12 -0500

    Made unit::Unit usable
    * Implmented unit::Unit.contains
    * Added unit::Unit.get_cells
    * Implemented fmt::Show for unit::Unit
    * Made unit::Unit public

--- src/unit.rs
+++ src/unit.rs
@@ -1,19 +1,39 @@
+use std::fmt;
+
 use cell::Cell;
 
-enum Unit {
+pub enum Unit {
     Column([Cell, ..9]),
     Row([Cell, ..9]),
     Grid([Cell, ..9])
 }
 
 impl Unit {
-    fn contains(&self, cell: Cell) -> bool {
-        let cells = match self {
-            &Unit::Column(cells) => cells,
-            &Unit::Row(cells) => cells,
-            &Unit::Grid(cells) => cells
+    pub fn get_cells(&self) -> [Cell, ..9] {
+        match *self {
+            Unit::Column(cells) => cells,
+            Unit::Row(cells) => cells,
+            Unit::Grid(cells) => cells
+        }
+    }
+
+    pub fn contains(&self, cell: Cell) -> bool {
+        let cells = self.get_cells();
+
+        for item in cells.iter() {
+            if *item == cell {
+                return true;
+            }
         };
 
         false
+
+    }
+}
+
+
+impl fmt::Show for Unit {
+    fn fmt(&self, out: &mut fmt::Formatter) -> fmt::Result {
+        write!(out, "{}", self.get_cells())
     }
 }
